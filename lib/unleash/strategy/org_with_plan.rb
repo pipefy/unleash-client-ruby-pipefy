0f75b14800150bb3a15be5cfdddd0d7eaf50dcdd
SalvaOliveira <101428598+SalvaOliveira@users.noreply.github.com>
2024-10-17 15:59:04 -0400

    Add paid org strategy (#17)
    * :sparkles: Add paid org strategy
    
    * :package: Bump version
    
    * :white_check_mark: Add test
    
    * :sparkles: Change strategy to be org with plan
    
    * :white_check_mark: Update tests
    
    * :white_check_mark: Update sym test
    
    * :sparkles: Make it case insensitive
    
    * :white_check_mark: Add spec for case changing

---
+++ lib/unleash/strategy/org_with_plan.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module Unleash
+  module Strategy
+    class OrgWithPlan < Base
+      PARAM = 'orgPlans'
+
+      def name
+        'orgWithPlan'
+      end
+
+      def is_enabled?(params = {}, context = nil)
+        allowed_plans(params).include?(current_org_plan(context))
+      end
+
+      private
+
+      def current_org_plan(context)
+        context&.properties&.fetch(:org_plan, "")&.downcase
+      end
+
+      def allowed_plans(params)
+        params[PARAM].downcase.split(',').map(&:strip)
+      end
+    end
+  end
+end
---
+++ spec/unleash/strategy/paid_org_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'unleash/context'
+require 'unleash/strategy/org_with_plan'
+
+RSpec.describe Unleash::Strategy::OrgWithPlan do
+  describe '#is_enabled?' do
+    let(:strategy) { Unleash::Strategy::OrgWithPlan.new }
+    let(:unleash_context) { Unleash::Context.new({ properties: org_plan_property }) }
+    let(:params) { { 'orgPlans' => "Pro,Enterprise" } }
+    let(:org_plan_property) { { 'org_plan' => 'Pro' } }
+
+    context 'with correct context as str' do
+      it 'should be enabled' do
+        expect(strategy.is_enabled?(params, unleash_context)).to be_truthy
+      end
+    end
+
+    context 'with correct context as sym' do
+      let(:org_plan_property) { { org_plan: 'Pro' } }
+  
+      it 'should be enabled' do
+        expect(strategy.is_enabled?(params, unleash_context)).to be_truthy
+      end
+    end
+
+    context 'with correct context as str and value with all upcase letters' do
+      let(:org_plan_property) { { org_plan: 'ENTERPRISE' } }
+
+      it 'should be enabled' do
+        expect(strategy.is_enabled?(params, unleash_context)).to be_truthy
+      end
+    end
+
+    context 'with wrong/falsey context' do
+      let(:org_plan_property) { { 'org_plan' => 'Free' } }
+  
+      it 'should not be enabled' do
+        expect(strategy.is_enabled?(params, unleash_context)).to be_falsey
+      end
+    end
+  end
+end
